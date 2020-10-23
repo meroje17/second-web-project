a93e516e6fa1512bca2043b9e6f7623757442d98
Jérôme Guèrin <jerome@MacBook-Pro-de-Jerome.local>
2020-10-23 14:51:13 +0200

    refactor(js doc): loop

--- script/script.js
+++ script/script.js
@@ -1,26 +1,20 @@
 let switchButton = document.querySelector('#checkbox');
-let monthly = document.querySelectorAll('.monthly');
-let annually = document.querySelectorAll('.annually');
-let buttons = document.querySelectorAll('.options a');
-let options = document.querySelectorAll('.options');
 
-let title3 = document.querySelectorAll('h3');
-let title2 = document.querySelectorAll('h2');
-let paragraphs1 = document.querySelectorAll('.options:nth-child(1) p');
-let paragraphs2 = document.querySelectorAll('.options:nth-child(2) p');
-let paragraphs3 = document.querySelectorAll('.options:nth-child(3) p');
+let elements = [];
+
+elements.push(...document.querySelectorAll('.monthly'));
+elements.push(...document.querySelectorAll('.annually'));
+elements.push(...document.querySelectorAll('.options a'));
+elements.push(...document.querySelectorAll('.options'));
+elements.push(...document.querySelectorAll('h3'));
+elements.push(...document.querySelectorAll('.options:nth-child(1) p'));
+elements.push(...document.querySelectorAll('.options:nth-child(2) p'));
+elements.push(...document.querySelectorAll('.options:nth-child(3) p'));
 
 switchButton.addEventListener('click', function() {
     let slider = switchButton.querySelector('#slider');
     slider.classList.toggle('selected');
-    for (var i = 0; i < 3; i++) {
-        monthly[i].classList.toggle('selected');
-        annually[i].classList.toggle('selected');
-        buttons[i].classList.toggle('selected');
-        options[i].classList.toggle('selected');
-        title3[i].classList.toggle('selected');
-        paragraphs1[i].classList.toggle('selected');
-        paragraphs2[i].classList.toggle('selected');
-        paragraphs3[i].classList.toggle('selected');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.toggle('selected');
     }
 });
